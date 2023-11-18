272b2ef6a39d29fa321ca3d801b0b05a0bc77e62
Rafasouzabreu <rafaelviking88@gmail.com>
2023-11-18 13:03:58 -0300

    feat: desenvolvimento de dtos e handles de erros

--- src/main/java/br/com/cotiinformatica/domain/services/ProdutoServiceImpl.java
+++ src/main/java/br/com/cotiinformatica/domain/services/ProdutoServiceImpl.java
@@ -1,20 +1,58 @@
 package br.com.cotiinformatica.domain.services;
+import java.math.BigDecimal;
 import java.util.List;
-
+import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.domain.entities.Categoria;
+import br.com.cotiinformatica.domain.entities.Fornecedor;
 import br.com.cotiinformatica.domain.entities.Produto;
 import br.com.cotiinformatica.domain.interfaces.ProdutoService;
+import br.com.cotiinformatica.dtos.ProdutoPostDto;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
+import br.com.cotiinformatica.repositories.FornecedorRepository;
 import br.com.cotiinformatica.repositories.ProdutoRepository;
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
+	@Autowired
+	FornecedorRepository fornecedorRepository;
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	@Override
-	public void create(Produto produto) throws Exception {
+	public void create(ProdutoPostDto dto) throws Exception {
+		
+		//buscando o fornecedor no banco de dados
+		Optional<Fornecedor> fornecedor = fornecedorRepository.findById(dto.getIdFornecedor());
+		//buscando a categoria no banco de dados
+		Optional<Categoria> categoria = categoriaRepository.findById(dto.getIdCategoria());
+		//verificando se o fornecer não foi encontrado
+		if(fornecedor.isEmpty())
+			throw new IllegalArgumentException("Erro. A fornecedor informado não existe.");
+		//verificando se a categorianão foi encontrada
+		if(categoria.isEmpty())
+			throw new IllegalArgumentException("Erro. A categoria informada não existe.");
+		//preencher os dados do produto
+		Produto produto = new Produto();
+		
+		produto.setId(UUID.randomUUID());
+		produto.setNome(dto.getNome());
+		produto.setPreco(new BigDecimal(dto.getPreco()));
+		produto.setQuantidade(dto.getQuantidade());
+		produto.setDescricao(dto.getDescricao());
+		produto.setFornecedor(fornecedor.get());
+		produto.setCategoria(categoria.get());
+		
+		//gravar no banco de dados
 		produtoRepository.save(produto);
+
 	}
 	@Override
 	public void update(Produto produto) throws Exception {
@@ -34,6 +72,5 @@ public class ProdutoServiceImpl implements ProdutoService {
 		return produtoRepository.findById(id).get();
 	}
 }
-
 
 
