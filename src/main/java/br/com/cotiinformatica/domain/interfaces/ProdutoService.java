2dcbe4e4087501b20277d3288543e6403aa38713
Rafasouzabreu <Rafaelviking88@gmail.com>
2023-11-25 13:06:28 -0300

    feat dto model mapper e endpoint produtos

--- src/main/java/br/com/cotiinformatica/domain/interfaces/ProdutoService.java
+++ src/main/java/br/com/cotiinformatica/domain/interfaces/ProdutoService.java
@@ -5,11 +5,12 @@ import java.util.UUID;
 
 import br.com.cotiinformatica.domain.entities.Produto;
 import br.com.cotiinformatica.dtos.ProdutoPostDto;
+import br.com.cotiinformatica.dtos.ProdutoPutDto;
 
 public interface ProdutoService {
 	void create(ProdutoPostDto dto) throws Exception;
 
-	void update(Produto protudo) throws Exception;
+	void update(ProdutoPutDto protudo) throws Exception;
 
 	void delete(UUID Id) throws Exception;
 	
--- src/test/java/br/com/cotiinformatica/ProjetoApiprodutosApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/ProjetoApiprodutosApplicationTests.java
@@ -1,4 +1,8 @@
+
+
 package br.com.cotiinformatica;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +11,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ProjetoApiprodutosApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getCategoriasTest() {
+		fail("Não implementado.");
 	}
 
+	@Test
+	void getFornecedoresTest() {
+		fail("Não implementado.");
+	}
+
+	@Test
+	void postProdutoTest() {
+		fail("Não implementado.");
+	}
+
+	@Test
+	void putProdutoTest() {
+		fail("Não implementado.");
+	}
+
+	@Test
+	void deleteProdutoTest() {
+		fail("Não implementado.");
+	}
+
+	@Test
+	void getProdutosTest() {
+		fail("Não implementado.");
+	}
 }
+
+
